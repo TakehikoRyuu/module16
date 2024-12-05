a3b089ac7eddf1d516be833f4fa27e4a2387010f
Андрей Шеремет <seheremet.and@yandex.ru>
2024-12-05 13:58:46 +0300

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -7,20 +7,20 @@ app = FastAPI()
 
 
 @app.get("/")
-async def welcome() -> dict:
-    return {"message": "Главная страница"}
+async def welcome() -> str:
+    return "Главная страница"
 
 
 @app.get("/user/admin")
-async def admin() -> dict:
-    return {"message": "Вы вошли как администратор"}
+async def admin() -> str:
+    return "Вы вошли как администратор"
 
 
 @app.get("/user/{user_id}")
-async def user_id(user_id: int = 123) -> dict:
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+async def user_id(user_id: int = 123) -> str:
+    return f"Вы вошли как пользователь № {user_id}"
 
-@app.get("/user/{username}/{age}")
-async def user_stats(username: str, age: int) -> dict:
-    return {"massage": f"Информация о пользователе.", "Имя": {username}, "Возраст": {age}}
+@app.get("/user")
+async def user_stats(username: str, age: int) -> str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
 
