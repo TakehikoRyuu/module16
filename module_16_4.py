a32aefb8b5c54c9a0cdc79dc4eb0a05a425adb54
Андрей Шеремет <seheremet.and@yandex.ru>
2024-12-19 11:31:51 +0300

    Update module_16_4.py

--- module_16_4.py
+++ module_16_4.py
@@ -1,40 +1,48 @@
 # Основы Fast Api и маршрутизация \ CRUD Запросы: Get, Post, Put Delete. \ Модели данных Pydantic
 # uvicorn module_16_4:app --reload
 
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException, Path
+from pydantic import BaseModel, Field, ValidationError
+from typing import Annotated
 
 app = FastAPI()
 
 
 class User(BaseModel):
     id: int
-    username: str
-    age: int
+    username: str = Field(min_length=3, max_length=30)
+    age: int = Field(ge=0, le=150)
 
 
 users = {}
 users_id = 0
-# default_user = User(id=1, username="Example", age=18)
-# users[str(default_user.id)] = default_user
 
 
-@app.get("/users")
-async def get_user() -> dict:
+@app.get("/users", response_model=dict[int, User])
+async def get_users() -> dict[int, User]:
     return users
 
 
-@app.post("/user/{username}/{age}")
-async def post_user(username: str, age: int) -> str:
-    global users_id
-    users_id += 1
-    user = User(id=users_id, username=username, age=age)
-    users[users_id] = user
-    return f"User {users_id} is registered"
+@app.post("/user/{username}/{age}", response_model=User)
+async def post_user(username: Annotated[str, Path(min_length=5, max_length=20,
+                                                   description="Enter username", example="UrbanUser")],
+                     age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) -> User:
+    try:
+        global users_id
+        users_id += 1
+        user = User(id=users_id, username=username, age=age)
+        users[users_id] = user
+        return f"User {users_id} is registered"
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=e.errors())
+
 
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def put_user(user_id: int, username: str, age: int) -> str:
+async def put_user(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example=10)],
+                   username: Annotated[str, Path(min_length=5, max_length=20,
+                                                 description="Enter username", example="UrbanUser")],
+                   age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) -> User:
     try:
         if user_id in users.keys():
             user = users[user_id]
@@ -46,7 +54,7 @@ async def put_user(user_id: int, username: str, age: int) -> str:
 
 
 @app.delete("/user/{user_id}")
-async def delete_user(user_id: int) -> str:
+async def delete_user(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example=10)]) -> User:
     try:
         users.pop(user_id)
         return f"User {user_id} has been deleted"
